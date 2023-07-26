fef68e11afe5c90ef05484f448942d7a8802c1f0
tanmaykapoor <tanmay.skater@gmail.com>
2023-07-26 14:42:38 +0530

    change env variables for all environments and remove unecessary config

--- config/config.js
+++ config/config.js
@@ -2,30 +2,27 @@ require("dotenv").config();
 
 module.exports = {
     development: {
-        username: process.env.DB_USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
-        host: process.env.DB_HOST,
+        username: process.env.DB_USER_DEV,
+        password: process.env.PASSWORD_DEV,
+        database: process.env.DATABASE_DEV,
+        host: process.env.DB_HOST_DEV,
         dialect: "postgresql",
-        define: {
-            timestamps: false,
-        },
     },
     test: {
-        username: process.env.DB_USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
-        host: process.env.DB_HOST,
+        username: process.env.DB_USER_TEST,
+        password: process.env.PASSWORD_TEST,
+        database: process.env.DATABASE_TEST,
+        host: process.env.DB_HOST_TEST,
         dialect: "postgresql",
         define: {
             timestamps: false,
         },
     },
     production: {
-        username: process.env.DB_USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
-        host: process.env.DB_HOST,
+        username: process.env.DB_USER_PROD,
+        password: process.env.PASSWORD_PROD,
+        database: process.env.DATABASE_PROD,
+        host: process.env.DB_HOST_PROD,
         dialect: "postgresql",
         define: {
             timestamps: false,
